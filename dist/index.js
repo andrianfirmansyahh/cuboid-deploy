600309197e056d5185f4649201b1f41a1b02d0ff
andrian firmansyah <andrian@cc-co.co>
2021-11-22 22:27:07 +0700

    adding material

--- dist/index.js
+++ dist/index.js
@@ -28,16 +28,16 @@ animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set(1,1,1);
+    camera = new THREE.PerspectiveCamera( 105, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.set(1,5,1);
     
-    // const gui = new dat.GUI();
-    // const animationsFolder = gui.addFolder('Animations')
-    // animationsFolder.open()
-
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xffffff );
-    scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+    // scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+
+    const gui = new dat.GUI();
+    const animationsFolder = gui.addFolder('Animations')
+    animationsFolder.open()
 
     // BACKGROUND MOUNTAIN
 
@@ -63,12 +63,31 @@ function init() {
     cube.rotation.y = 3.58;
     scene.add( cube );
 
+    //LIGHTS
 
-    const light = new THREE.PointLight( 0xffffff, 1, 100 );
-    light.position.set( 0, 19, -9 );
-    light.intensity = 2;
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff );
+    hemiLight.position.set( 0, 30, 0 );
+    hemiLight.intensity = 4;
+    scene.add( hemiLight );
+
+    const dirLight = new THREE.DirectionalLight( 0xffffff );
+    dirLight.position.set( - 3, 10, - 10 );
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 2;
+    dirLight.shadow.camera.bottom = - 2;
+    dirLight.shadow.camera.left = - 2;
+    dirLight.shadow.camera.right = 2;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 40;
+    scene.add( dirLight );
+
+    // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+
+    // const light = new THREE.PointLight( 0xffffff, 1, 100 );
+    // light.position.set( 0, 19, -9 );
+    // light.intensity = 2;
     
-    scene.add( light );
+    // scene.add( light );
 
     // gui.add(light.position, 'x')
     // gui.add(light.position, 'y')
@@ -78,11 +97,11 @@ function init() {
     // const pointLightHelper = new THREE.PointLightHelper(light, 2);
     // scene.add(pointLightHelper)
 
-    const light2 = new THREE.PointLight( 0xffffff, 1, 100 );
-    light2.position.set(1, -22, -93 );
-    light2.intensity = 4;
+    // const light2 = new THREE.PointLight( 0xffffff, 1, 100 );
+    // light2.position.set(1, -22, -93 );
+    // light2.intensity = 4;
     
-    scene.add( light2 );
+    // scene.add( light2 );
 
     // gui.add(light2.position, 'x')
     // gui.add(light2.position, 'y')
@@ -92,11 +111,11 @@ function init() {
     // const pointLightHelper2 = new THREE.PointLightHelper(light2, 2);
     // scene.add(pointLightHelper2)
 
-    const light3 = new THREE.PointLight( 0xFFF0B3, 1, 100 );
-    light3.position.set(-0.1, 0.9, -239 );
-    light3.intensity = 2;
+    // const light3 = new THREE.PointLight( 0xFFF0B3, 1, 100 );
+    // light3.position.set(-0.1, 0.9, -239 );
+    // light3.intensity = 2;
     
-    scene.add( light3 );
+    // scene.add( light3 );
 
     // gui.add(light3.position, 'x')
     // gui.add(light3.position, 'y')
@@ -212,34 +231,34 @@ function init() {
     //OBJECT
     //GLTFLoader
     const loader = new GLTFLoader()
-    loader.load('spacea/spacea.gltf',function ( gltf ) {
-            const mars = gltf.scene;
-            mars.scale.set(1,1,1)
-            mars.position.set(1,-2.75,-30)
-            scene.add( mars );
+    // loader.load('pa/PA.gltf',function ( gltf ) {
+    //         const mars = gltf.scene;
+    //         mars.scale.set(1,1,1)
+    //         mars.position.set(0,0,0)
+    //         scene.add( mars );
 
-            gltf.animations; // Array<THREE.AnimationClip>
-            gltf.scene; // THREE.Group
-            gltf.scenes; // Array<THREE.Group>
-            gltf.cameras; // Array<THREE.Camera>
-            gltf.asset; // Object
-        },
+    //         gltf.animations; // Array<THREE.AnimationClip>
+    //         gltf.scene; // THREE.Group
+    //         gltf.scenes; // Array<THREE.Group>
+    //         gltf.cameras; // Array<THREE.Camera>
+    //         gltf.asset; // Object
+    //     },
 
-        // called while loading is progressing
-        function ( xhr ) {
+    //     // called while loading is progressing
+    //     function ( xhr ) {
 
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    //         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-        },
-        // called when loading has errors
-        function ( error ) {
+    //     },
+    //     // called when loading has errors
+    //     function ( error ) {
 
-            console.log( 'An error happened' );
+    //         console.log( 'An error happened' );
 
-        }
-    );
+    //     }
+    // );
 
-    loader.load('spaceb/spaceb.gltf',function ( gltf ) {
+    loader.load('gltf5/gltf5.gltf',function ( gltf ) {
         const mars2 = gltf.scene;
         mars2.scale.set(1,1,1)
         mars2.position.set(1,-2.75,-30)
@@ -267,48 +286,20 @@ function init() {
 );
 
 
-loader.load('spacec/spacec.gltf',function ( gltf ) {
-    const mars3 = gltf.scene;
-    mars3.scale.set(1,1,1)
-    mars3.position.set(1,-2.75,-30)
-    scene.add( mars3 );
+// loader.load('spacec/spacec.gltf',function ( gltf ) {
+//     const mars3 = gltf.scene;
+//     mars3.scale.set(1,1,1)
+//     mars3.position.set(1,-2.75,-30)
+//     scene.add( mars3 );
 
-    gltf.animations; // Array<THREE.AnimationClip>
-    gltf.scene; // THREE.Group
-    gltf.scenes; // Array<THREE.Group>
-    gltf.cameras; // Array<THREE.Camera>
-    gltf.asset; // Object
-},
+//     gltf.animations; // Array<THREE.AnimationClip>
+//     gltf.scene; // THREE.Group
+//     gltf.scenes; // Array<THREE.Group>
+//     gltf.cameras; // Array<THREE.Camera>
+//     gltf.asset; // Object
+// },
 
-// called while loading is progressing
-function ( xhr ) {
-
-    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-},
-// called when loading has errors
-function ( error ) {
-
-    console.log( 'An error happened' );
-
-}
-);
-
-// loader.load('logo/logo.gltf', function ( gltf ){
-//     scene.add( gltf.scene );
-//     logo = gltf.scene.children[0];
-//     logo.position.set(0,0,0)
-//     logo.scale.set(0,0,0);
-
-//     gui.add(logo.position, 'x')
-//     gui.add(logo.position, 'y')
-//     gui.add(logo.position, 'z')
-//     gui.add(logo.scale, 'x')
-//     gui.add(logo.scale, 'y')
-//     gui.add(logo.scale, 'z')
-// }),
-
-// called while loading is progressing
+// // called while loading is progressing
 // function ( xhr ) {
 
 //     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -320,7 +311,21 @@ function ( error ) {
 //     console.log( 'An error happened' );
 
 // }
+// );
 
+// loader.load('boots/boots.gltf',function ( gltf ) {
+//     scene.add( gltf.scene );
+//     boots = gltf.scene.children[0];
+//     boots.position.set(0,5,0);
+//     boots.scale.set(1,1,1);
+
+//     gui.add(boots.position, 'x')
+//     gui.add(boots.position, 'y')
+//     gui.add(boots.position, 'z')
+//     gui.add(boots.scale, 'x')
+//     gui.add(boots.scale, 'y')
+//     gui.add(boots.scale, 'z')
+// });
 
 
     //
@@ -362,8 +367,8 @@ function animate() {
 
         const delta = ( time - prevTime ) / 1000;
 
-        velocity.x -= velocity.x * 10.0 * delta;
-        velocity.z -= velocity.z * 10.0 * delta;
+        velocity.x -= velocity.x * 45.0 * delta;
+        velocity.z -= velocity.z * 45.0 * delta;
 
         velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
 
