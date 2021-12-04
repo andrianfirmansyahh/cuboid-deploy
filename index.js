a848057f19a71f1a4f1f4d0798240ff77c31f3ab
andrian firmansyah <andrian@cc-co.co>
2021-12-04 17:33:45 +0700

    Updates

--- index.js
+++ index.js
@@ -1507,6 +1507,16 @@ function controls( deltaTime ) {
 
 }
 
+function teleportPlayerIfOob(){
+    if (camera.position.y <= -25){
+        playerCollider.start.set( 0, 0.35, 0 );
+        playerCollider.end.set( 0, 1, 0 );
+        playerCollider.radius =  0.35;
+        camera.position.copy( playerCollider.end );
+        camera.rotation.set( 0, 0, 0 );
+    }
+}
+
 function animate() {
 
     const deltaTime = Math.min( 0.05, clock.getDelta() ) / STEPS_PER_FRAME;
@@ -1521,6 +1531,8 @@ function animate() {
         updatePlayer( deltaTime );
 
         updateSpheres( deltaTime );
+
+        teleportPlayerIfOob();
 
     }
 
